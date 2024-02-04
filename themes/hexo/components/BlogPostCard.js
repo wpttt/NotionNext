92a7be825ae4bb62dc8c14fdce49e99844e272d6
Wang Pt <wpttt@msn.cn>
2024-02-04 16:13:16 +0800

    update card link

--- themes/hexo/components/BlogPostCard.js
+++ themes/hexo/components/BlogPostCard.js
@@ -17,36 +17,36 @@ const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
   return (
 
         <div className={`${siteConfig('HEXO_POST_LIST_COVER_HOVER_ENLARGE', null, CONFIG) ? ' hover:scale-110 transition-all duration-150' : ''}`} >
-            <div key={post.id}
-                data-aos="fade-up"
-                data-aos-easing="ease-in-out"
-                data-aos-duration="800"
-                data-aos-once="false"
-                data-aos-anchor-placement="top-bottom"
-                id='blog-post-card'
-                className={`group md:h-56 w-full flex justify-between md:flex-row flex-col-reverse ${siteConfig('HEXO_POST_LIST_IMG_CROSSOVER', null, CONFIG) && index % 2 === 1 ? 'md:flex-row-reverse' : ''}
-                    overflow-hidden border dark:border-black rounded-xl bg-white dark:bg-hexo-black-gray`}>
-               
-                {/* 文字内容 */}
-                <BlogPostCardInfo index={index} post={post} showPageCover={showPageCover} showPreview={showPreview} showSummary={showSummary} />
+            <Link
+                href={url}
+                passHref
+                className={`line-clamp-2 replace cursor-pointer text-2xl ${showPreview ? 'text-center' : ''
+                    } leading-tight font-normal text-gray-600 dark:text-gray-100 hover:text-indigo-700 dark:hover:text-indigo-400`}>
 
-                {/* 图片封面 */}
-                {showPageCover && (
-                    <div className="md:w-5/12 overflow-hidden">
-                        <Link href={url} passHref legacyBehavior>
-                        <LazyImage priority={index === 1} src={post?.pageCoverThumbnail} className='h-56 w-full object-cover object-center group-hover:scale-110 duration-500' />
-                        </Link>
-                    </div>
-                )}
-                <Link
-                    href={url}
-                    passHref
-                    className={`line-clamp-2 replace cursor-pointer text-2xl ${showPreview ? 'text-center' : ''
-                        } leading-tight font-normal text-gray-600 dark:text-gray-100 hover:text-indigo-700 dark:hover:text-indigo-400`}>
+                <div key={post.id}
+                    data-aos="fade-up"
+                    data-aos-easing="ease-in-out"
+                    data-aos-duration="800"
+                    data-aos-once="false"
+                    data-aos-anchor-placement="top-bottom"
+                    id='blog-post-card'
+                    className={`group md:h-56 w-full flex justify-between md:flex-row flex-col-reverse ${siteConfig('HEXO_POST_LIST_IMG_CROSSOVER', null, CONFIG) && index % 2 === 1 ? 'md:flex-row-reverse' : ''}
+                        overflow-hidden border dark:border-black rounded-xl bg-white dark:bg-hexo-black-gray`}>
+                        
+                    {/* 文字内容 */}
+                    <BlogPostCardInfo index={index} post={post} showPageCover={showPageCover} showPreview={showPreview} showSummary={showSummary} />
 
-                </Link>
+                    {/* 图片封面 */}
+                    {showPageCover && (
+                        <div className="md:w-5/12 overflow-hidden">
+                            <Link href={url} passHref legacyBehavior>
+                            <LazyImage priority={index === 1} src={post?.pageCoverThumbnail} className='h-56 w-full object-cover object-center group-hover:scale-110 duration-500' />
+                            </Link>
+                        </div>
+                    )}
 
-            </div>
+                </div>
+            </Link>
 
         </div>
 
